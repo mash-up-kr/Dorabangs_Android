04ee7d066a9c525ca1e0f61560f69f9cea916e0d
Ahn-seokjoo <tjrwn0716@naver.com>
2024-05-13 00:01:51 +0900

    [feature] flipper 세팅 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,9 +23,20 @@ android {
         }
     }
 
+    buildFeatures {
+        buildConfig = true
+    }
+
     buildTypes {
-        release {
+        debug {
+            isDebuggable = true
+            applicationIdSuffix = ".debug"
             isMinifyEnabled = false
+            buildConfigField("Boolean", "DEBUG_MODE", "true")
+        }
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -61,4 +72,8 @@ dependencies {
 
     implementation(libs.hilt.android)
     kapt(libs.hilt.compiler)
+    debugImplementation(libs.flipper)
+    debugImplementation(libs.soloader)
+    debugImplementation(libs.flipper.network)
+    releaseImplementation(libs.noop)
 }
