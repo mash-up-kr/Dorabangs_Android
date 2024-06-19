28f7e513130f3f0f7f53428eea926b684a951516
fbghgus123 <fbghgus123@naver.com>
2024-06-19 23:38:44 +0900

    [feature] 디자인 시스템 radius 추가

---
+++ core/designsystem/src/main/java/com/mashup/dorabangs/core/designsystem/theme/Radius.kt
@@ -0,0 +1,16 @@
+package com.mashup.dorabangs.core.designsystem.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.unit.dp
+
+object DoraRoundTokens {
+    val Round99 = RoundedCornerShape(99.dp)
+    val Round12 = RoundedCornerShape(12.dp)
+    val Round8 = RoundedCornerShape(8.dp)
+    val Round4 = RoundedCornerShape(4.dp)
+}
+
+object BtnMaxRoundTokens {
+    val FullButtonWidthRadius = DoraRoundTokens.Round99
+    val MediumButtonWidthRadius = DoraRoundTokens.Round12
+}
