636bae44b28c1669b53a34746fbdfbe2f6d36584
ddyeon <cab05102@naver.com>
2024-06-24 23:24:52 +0900

    [feat] 홈화면 navigation 생성

---
+++ feature/home/src/main/java/com/mashup/dorabangs/feature/navigation/HomeNavigation.kt
@@ -0,0 +1,18 @@
+package com.mashup.dorabangs.feature.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import com.mashup.core.navigation.NavigationRoute
+import com.mashup.dorabangs.feature.home.HomeRoute
+
+fun NavController.navigateToHome(navOptions: NavOptions) = navigate(NavigationRoute.HomeScreen.route, navOptions)
+
+fun NavGraphBuilder.homeScreen() {
+    composable(
+        route = NavigationRoute.HomeScreen.route,
+    ) {
+        HomeRoute()
+    }
+}
