a9fb7f582ba6f7627451c8e32e6f5bfab8534253
ddyeon <cab05102@naver.com>
2024-06-29 22:58:22 +0900

    [feat] 보관함 모델 생성

---
+++ feature/storage/src/main/java/com/mashup/dorabangs/feature/storage/storagedetail/model/StorageDetailSort.kt
@@ -0,0 +1,8 @@
+package com.mashup.dorabangs.feature.storage.storagedetail.model
+
+import com.mashup.dorabangs.feature.storage.R
+
+enum class StorageDetailSort(val btnName: Int) {
+    LATEST(R.string.storage_detail_sort_latest),
+    PAST(R.string.storage_detail_sort_past),
+}
