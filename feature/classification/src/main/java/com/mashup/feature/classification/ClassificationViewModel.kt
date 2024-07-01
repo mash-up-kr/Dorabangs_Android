81c3edd0ed93b196b74252dc9956fed30ab96a99
ddyeon <cab05102@naver.com>
2024-07-01 23:34:30 +0900

    [feat] 분류 viewmodel 및 state 생성

---
+++ feature/classification/src/main/java/com/mashup/feature/classification/ClassificationViewModel.kt
@@ -0,0 +1,28 @@
+package com.mashup.feature.classification
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import org.orbitmvi.orbit.ContainerHost
+import org.orbitmvi.orbit.syntax.simple.intent
+import org.orbitmvi.orbit.syntax.simple.postSideEffect
+import org.orbitmvi.orbit.viewmodel.container
+import javax.inject.Inject
+
+@HiltViewModel
+class ClassificationViewModel @Inject constructor(): ViewModel(), ContainerHost<ClassificationState, ClassificationSideEffect> {
+    override val container = container<ClassificationState, ClassificationSideEffect>(ClassificationState())
+
+    fun changeCategory() = intent {}
+
+    fun moveAllItems() = intent {
+        postSideEffect(ClassificationSideEffect.OnClickAllItemMove)
+    }
+
+    fun moveSelectedItem(idx: Int) = intent {
+        postSideEffect(ClassificationSideEffect.OnClickSelectItemMove(idx = idx))
+    }
+
+    fun deleteSelectedItem(idx: Int) = intent {
+        postSideEffect(ClassificationSideEffect.OnClickDeleteCard(idx = idx))
+    }
+}
