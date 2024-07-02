22d2a2516cc88a4e4b969ad645edf237b451474d
ddyeon <cab05102@naver.com>
2024-07-02 21:32:12 +0900

    [fix] 클릭 시 사이드 이펙트 수정

--- feature/classification/src/main/java/com/mashup/feature/classification/ClassificationSideEffect.kt
+++ feature/classification/src/main/java/com/mashup/feature/classification/ClassificationSideEffect.kt
@@ -1,7 +1,2 @@
 package com.mashup.feature.classification
-
-sealed class ClassificationSideEffect {
-    data class OnClickDeleteCard(val idx: Int) : ClassificationSideEffect()
-    object OnClickAllItemMove : ClassificationSideEffect()
-    data class OnClickSelectItemMove(val idx: Int) : ClassificationSideEffect()
-}
+sealed class ClassificationSideEffect
--- feature/classification/src/main/java/com/mashup/feature/classification/ClassificationViewModel.kt
+++ feature/classification/src/main/java/com/mashup/feature/classification/ClassificationViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import org.orbitmvi.orbit.ContainerHost
 import org.orbitmvi.orbit.syntax.simple.intent
-import org.orbitmvi.orbit.syntax.simple.postSideEffect
 import org.orbitmvi.orbit.viewmodel.container
 import javax.inject.Inject
 
@@ -15,14 +14,11 @@ class ClassificationViewModel @Inject constructor() : ViewModel(), ContainerHost
     fun changeCategory() = intent {}
 
     fun moveAllItems() = intent {
-        postSideEffect(ClassificationSideEffect.OnClickAllItemMove)
     }
 
     fun moveSelectedItem(idx: Int) = intent {
-        postSideEffect(ClassificationSideEffect.OnClickSelectItemMove(idx = idx))
     }
 
     fun deleteSelectedItem(idx: Int) = intent {
-        postSideEffect(ClassificationSideEffect.OnClickDeleteCard(idx = idx))
     }
 }
