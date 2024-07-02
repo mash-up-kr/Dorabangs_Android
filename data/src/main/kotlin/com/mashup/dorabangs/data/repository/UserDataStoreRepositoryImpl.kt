95a166d144ff13852a15d1b2f99da06dd1cd72f0
ddyeon <cab05102@naver.com>
2024-07-02 22:23:02 +0900

    [feat] userdatastore repository 생성

---
+++ data/src/main/kotlin/com/mashup/dorabangs/data/repository/UserDataStoreRepositoryImpl.kt
@@ -0,0 +1,27 @@
+package com.mashup.dorabangs.data.repository
+
+import com.mashup.dorabangs.data.datasource.local.UserPreferenceDataSource
+import kotlinx.coroutines.flow.Flow
+import repository.UserDataStoreRepository
+import javax.inject.Inject
+
+class UserDataStoreRepositoryImpl @Inject constructor(
+    private val dataSource: UserPreferenceDataSource
+
+): UserDataStoreRepository {
+    override suspend fun setUserAccessToken(accessToken: String) {
+        dataSource.setUserAccessToken(accessToken = accessToken)
+    }
+
+    override fun getUserAccessToken(): Flow<String> {
+        return dataSource.getUserAccessToken()
+    }
+
+    override suspend fun setIsFirstEntry(isFirst: Boolean) {
+        dataSource.setIsFirstEntry(isFirst = isFirst)
+    }
+
+    override fun getIsFirstEntry(): Flow<Boolean> {
+        return dataSource.getIsFirstEntry()
+    }
+}
