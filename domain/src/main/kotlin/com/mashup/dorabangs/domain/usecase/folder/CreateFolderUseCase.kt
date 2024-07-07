c3ca7553e9e4405583f9d927f4f1fd46e2408313
ddyeon <cab05102@naver.com>
2024-07-07 23:24:02 +0900

    [feat] 폴더 생성 및 수정 usecase 추가

--- domain/src/main/kotlin/com/mashup/dorabangs/domain/usecase/folder/CreateFolderUseCase.kt
+++ domain/src/main/kotlin/com/mashup/dorabangs/domain/usecase/folder/CreateFolderUseCase.kt
@@ -1,12 +1,14 @@
 package com.mashup.dorabangs.domain.usecase.folder
 
-import com.mashup.dorabangs.domain.model.FolderList
+import com.mashup.dorabangs.domain.model.CreateCompleteFolderInfo
+import com.mashup.dorabangs.domain.model.CreateFolder
 import com.mashup.dorabangs.domain.repository.FolderRepository
 import javax.inject.Inject
 
 class CreateFolderUseCase @Inject constructor(
     private val folderRepository: FolderRepository
 ){
-    suspend operator fun invoke() {
+    suspend operator fun invoke(folderList: CreateFolder): CreateCompleteFolderInfo {
+        return folderRepository.createFolder(createFolder = folderList)
     }
 }
--- domain/src/main/kotlin/com/mashup/dorabangs/domain/usecase/folder/EditFolderNameUseCase.kt
+++ domain/src/main/kotlin/com/mashup/dorabangs/domain/usecase/folder/EditFolderNameUseCase.kt
@@ -1,4 +1,14 @@
 package com.mashup.dorabangs.domain.usecase.folder
 
-class EditFolderNameUseCase {
+import com.mashup.dorabangs.domain.model.EditCompleteFolderInfo
+import com.mashup.dorabangs.domain.model.EditFolder
+import com.mashup.dorabangs.domain.repository.FolderRepository
+import javax.inject.Inject
+
+class EditFolderNameUseCase @Inject constructor(
+    private val folderRepository: FolderRepository
+){
+    suspend operator fun invoke(folderName: EditFolder): EditCompleteFolderInfo {
+        return folderRepository.editFolderName(editFolder = folderName)
+    }
 }
