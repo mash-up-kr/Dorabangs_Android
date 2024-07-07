3723bccfab48557c1a68acd7c347b50de11ffba9
Ahn-seokjoo <tjrwn0716@naver.com>
2024-07-07 23:09:20 +0900

    [#66 feature] 외부에서 공유하기 기능 추가

--- app/src/main/java/com/mashup/dorabangs/MainActivity.kt
+++ app/src/main/java/com/mashup/dorabangs/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.mashup.dorabangs
 
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.provider.Settings
 import androidx.activity.ComponentActivity
@@ -32,6 +34,28 @@ class MainActivity : ComponentActivity() {
             DorabangsTheme {
                 DoraApp()
             }
+        }
+        handleShare()
+    }
+
+    private fun handleShare() {
+        val type = intent.type
+        if (Intent.ACTION_SEND == intent.action && type != null) {
+            if ("text/plain" == type) {
+                handleSendText()
+            }
+        }
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        handleSendText()
+    }
+
+    private fun handleSendText() {
+        val linkUrl = intent.getStringExtra(Intent.EXTRA_TEXT).orEmpty()
+        if (linkUrl.isNotBlank()) {
+            println("tjrwn link url is ${linkUrl}")
         }
     }
 }
