0f2dcb146a8976bfbc38de27ef05d84f1d7c9b55
ddyeon <cab05102@naver.com>
2024-07-07 16:46:04 +0900

    [feat] 폴더생성 navigation 생성

---
+++ feature/home/src/main/java/com/mashup/dorabangs/feature/navigation/HomeCreateFolderNavigation.kt
@@ -0,0 +1,23 @@
+package com.mashup.dorabangs.feature.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import com.mashup.core.navigation.NavigationRoute
+import com.mashup.dorabangs.feature.createfolder.HomeCreateFolderRoute
+
+fun NavController.navigateToHomeCrateFolder(navOptions: NavOptions? = null) =
+    navigate(NavigationRoute.HomeScreen.HomeCreateFolder.route, navOptions)
+
+fun NavGraphBuilder.homeCreateFolderNavigation(
+    onClickBackIcon: () -> Unit,
+) {
+    composable(
+        route = NavigationRoute.HomeScreen.HomeCreateFolder.route,
+    ) {
+        HomeCreateFolderRoute(
+            onClickBackIcon = {},
+        )
+    }
+}
