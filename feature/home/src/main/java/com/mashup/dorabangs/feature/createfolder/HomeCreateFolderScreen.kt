98d27bdef5111736db061080c6e09f85031e047f
ddyeon <cab05102@naver.com>
2024-07-07 16:45:29 +0900

    [feat] 홈화면 폴더 생성 화면 추가

---
+++ feature/home/src/main/java/com/mashup/dorabangs/feature/createfolder/HomeCreateFolderScreen.kt
@@ -0,0 +1,73 @@
+package com.mashup.dorabangs.feature.createfolder
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.mashup.dorabangs.core.designsystem.component.buttons.DoraButtons
+import com.mashup.dorabangs.core.designsystem.component.textfield.DoraTextField
+import com.mashup.dorabangs.core.designsystem.component.topbar.DoraTopBar
+import com.mashup.dorabangs.core.designsystem.theme.LinkSaveColorTokens
+import com.mashup.dorabangs.home.R
+
+@Composable
+fun HomeCreateFolderRoute(
+    onClickBackIcon: () -> Unit,
+) {
+    HomeCreateFolderScreen(
+        onClickBackIcon = onClickBackIcon,
+        onClickSaveButton = {},
+    )
+}
+
+@Composable
+fun HomeCreateFolderScreen(
+    onClickBackIcon: () -> Unit,
+    onClickSaveButton: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = LinkSaveColorTokens.ContainerColor),
+    ) {
+        DoraTopBar.BackNavigationTopBar(
+            modifier = Modifier,
+            title = stringResource(id = R.string.home_create_folder_title),
+            isTitleCenter = true,
+            onClickBackIcon = onClickBackIcon,
+        )
+        Spacer(modifier = Modifier.height(height = 24.dp))
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(horizontal = 20.dp),
+        ) {
+            DoraTextField(
+                text = "",
+                hintText = stringResource(id = R.string.home_create_folder_hint),
+                labelText = stringResource(id = R.string.home_create_folder_label),
+                helperText = stringResource(id = R.string.home_create_folder_helper),
+                helperEnabled = true,
+                counterEnabled = true,
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            DoraButtons.DoraBtnMaxFull(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 20.dp),
+                buttonText = stringResource(id = R.string.home_create_folder_save),
+                enabled = true,
+                onClickButton = onClickSaveButton,
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+        }
+    }
+}
