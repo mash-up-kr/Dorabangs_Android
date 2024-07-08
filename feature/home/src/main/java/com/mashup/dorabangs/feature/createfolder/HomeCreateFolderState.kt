04971e072320fcafc63563cb08c75007992f76f9
ddyeon <cab05102@naver.com>
2024-07-08 22:37:46 +0900

    [feat] 홈 폴더 생성 state 생성

---
+++ feature/home/src/main/java/com/mashup/dorabangs/feature/createfolder/HomeCreateFolderState.kt
@@ -0,0 +1,7 @@
+package com.mashup.dorabangs.feature.createfolder
+
+data class HomeCreateFolderState(
+    val folderName: String = "",
+    val helperEnable: Boolean = false,
+    val helperMessage: String = ""
+)
