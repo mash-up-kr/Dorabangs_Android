127e8b5818007e2f827b26e5d33577a8b74233e6
fbghgus123 <fbghgus123@naver.com>
2024-07-18 22:09:33 +0900

    [feature] AI 분류 UseCase 구현

---
+++ domain/src/main/kotlin/com/mashup/dorabangs/domain/usecase/aiclassification/GetAIClassificationPostsByFolderUseCase.kt
@@ -0,0 +1,22 @@
+package com.mashup.dorabangs.domain.usecase.aiclassification
+
+import com.mashup.dorabangs.domain.repository.AIClassificationRepository
+import javax.inject.Inject
+
+class GetAIClassificationPostsByFolderUseCase @Inject constructor(
+    private val aiClassificationRepository: AIClassificationRepository,
+) {
+
+    suspend operator fun invoke(
+        folderId: String,
+        page: Int? = null,
+        limit: Int? = null,
+        order: String? = null,
+    ) =
+        aiClassificationRepository.getAIClassificationPostsByFolder(
+            folderId = folderId,
+            page = page,
+            limit = limit,
+            order = order,
+        )
+}
