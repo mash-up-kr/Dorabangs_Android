2d36c8c018c9318e79c8aa583ca350a360286945
ddyeon <cab05102@naver.com>
2024-07-19 00:48:09 +0900

    [feat] viewModel paging 데이터 가져오기 로직 추가

--- feature/storage/src/main/java/com/mashup/dorabangs/feature/storage/storagedetail/StorageDetailViewModel.kt
+++ feature/storage/src/main/java/com/mashup/dorabangs/feature/storage/storagedetail/StorageDetailViewModel.kt
@@ -1,10 +1,20 @@
 package com.mashup.dorabangs.feature.storage.storagedetail
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import androidx.paging.map
+import com.mashup.dorabangs.core.coroutine.doraLaunch
+import com.mashup.dorabangs.domain.usecase.folder.GetSavedLinksFromFolderUseCase
 import com.mashup.dorabangs.feature.storage.storagedetail.model.StorageDetailSideEffect
 import com.mashup.dorabangs.feature.storage.storagedetail.model.StorageDetailSort
 import com.mashup.dorabangs.feature.storage.storagedetail.model.StorageDetailState
+import com.mashup.dorabangs.feature.storage.storagedetail.model.toUiModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import org.orbitmvi.orbit.ContainerHost
 import org.orbitmvi.orbit.syntax.simple.intent
 import org.orbitmvi.orbit.syntax.simple.reduce
@@ -12,8 +22,25 @@ import org.orbitmvi.orbit.viewmodel.container
 import javax.inject.Inject
 
 @HiltViewModel
-class StorageDetailViewModel @Inject constructor() : ViewModel(), ContainerHost<StorageDetailState, StorageDetailSideEffect> {
+class StorageDetailViewModel @Inject constructor(
+    private val savedLinksFromFolderUseCase: GetSavedLinksFromFolderUseCase,
+) : ViewModel(), ContainerHost<StorageDetailState, StorageDetailSideEffect> {
     override val container = container<StorageDetailState, StorageDetailSideEffect>(StorageDetailState())
+
+    fun getSavedLinkFromFolder(folderId: String, order: String, unread: Boolean) =
+        viewModelScope.doraLaunch {
+            val pagingData = savedLinksFromFolderUseCase.invoke(folderId = folderId, order = order, unread = unread)
+                .cachedIn(viewModelScope).map { pagedData ->
+                    pagedData.map { savedLinkInfo -> savedLinkInfo.toUiModel() }
+                }.stateIn(
+                    scope = viewModelScope,
+                    started = SharingStarted.Lazily,
+                    initialValue = PagingData.empty(),
+                )
+            intent {
+                reduce { state.copy(pagingList = pagingData) }
+            }
+        }
 
     fun changeSelectedTabIdx(selectedIdx: Int) = intent {
         reduce {
--- feature/storage/src/main/java/com/mashup/dorabangs/feature/storage/storagedetail/model/StorageDetailState.kt
+++ feature/storage/src/main/java/com/mashup/dorabangs/feature/storage/storagedetail/model/StorageDetailState.kt
@@ -1,6 +1,10 @@
 package com.mashup.dorabangs.feature.storage.storagedetail.model
 
+import androidx.paging.PagingData
 import com.mashup.dorabangs.core.designsystem.component.card.FeedCardUiModel
+import com.mashup.dorabangs.domain.model.SavedLinkDetailInfo
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
 
 data class StorageDetailState(
     val title: String = "나중에 읽을 링크",
@@ -9,8 +13,19 @@ data class StorageDetailState(
     val selectedIdx: Int = 0,
     val isLatestSort: Boolean = false,
     val postList: List<FeedCardUiModel> = listOf(),
+    val pagingList: Flow<PagingData<FeedCardUiModel>> = emptyFlow(),
 ) {
     companion object {
         fun getDefaultTabTitleList() = listOf(StorageDetailTab.ALL, StorageDetailTab.UNREAD)
     }
+}
+
+fun SavedLinkDetailInfo.toUiModel(): FeedCardUiModel {
+    return FeedCardUiModel(
+        title = this.title,
+        content = this.description,
+        createdAt = this.createdAt,
+        keywordList = this.keywords.map { it.name },
+        thumbnail = this.url.toInt(),
+    )
 }
