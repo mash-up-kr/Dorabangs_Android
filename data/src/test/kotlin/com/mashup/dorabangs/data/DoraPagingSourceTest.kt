4b3036cba51fb24b21a4dd52bcf285d09bb02193
ddyeon <cab05102@naver.com>
2024-07-20 20:41:42 +0900

    [feat] pagingSource 테스트 파일

--- data/src/test/kotlin/com/mashup/dorabangs/data/DoraPagingSourceTest.kt
+++ data/src/test/kotlin/com/mashup/dorabangs/data/DoraPagingSourceTest.kt
@@ -6,16 +6,14 @@ import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
 import org.junit.Test
 
-
 class DoraPagingSourceTest {
 
     val pagingSource: DoraPagingSource<SavedLinkDetailInfo> = mockk()
 
     @Test
     fun `페이징로드테스트`() = runTest {
+        // Excutor를 mocking한다. -> load안에 넣는다~~
 
-        //Excutor를 mocking한다. -> load안에 넣는다~~
-
-        //pagingSource.load()
+        // pagingSource.load()
     }
 }
