d874e20e43ec0b8e3f23a521a558ba3a6f4b729a
ddyeon <cab05102@naver.com>
2024-07-20 20:42:13 +0900

    [feat] postFolder 모델 타입 변경

--- data/src/main/kotlin/com/mashup/dorabangs/data/model/PostInfoRequestModel.kt
+++ data/src/main/kotlin/com/mashup/dorabangs/data/model/PostInfoRequestModel.kt
@@ -6,7 +6,7 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class PostInfoRequestModel(
     val isFavorite: Boolean,
-    val readAt: String,
+    val readAt: Boolean,
 )
 
 fun PostInfoRequestModel.toDomain() = PostInfo(
