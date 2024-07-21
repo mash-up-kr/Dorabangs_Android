bc4593de01990da04f392b9ff8c741b3105d68a3
fbghgus123 <fbghgus123@naver.com>
2024-07-21 23:30:43 +0900

    [fix] 바텀 시트 둥근거 삭제

--- core/designsystem/src/main/java/com/mashup/dorabangs/core/designsystem/component/bottomnavigation/BottomNavigation.kt
+++ core/designsystem/src/main/java/com/mashup/dorabangs/core/designsystem/component/bottomnavigation/BottomNavigation.kt
@@ -1,11 +1,9 @@
 package com.mashup.dorabangs.core.designsystem.component.bottomnavigation
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
@@ -13,12 +11,10 @@ import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.mashup.dorabangs.core.designsystem.theme.DoraColorTokens
 import com.mashup.dorabangs.core.designsystem.theme.DoraTypoTokens
 
@@ -32,8 +28,6 @@ fun DoraBottomNavigation(
         Modifier
             .fillMaxWidth()
             .background(Color.Transparent)
-            .border(width = 1.dp, color = DoraColorTokens.G2, shape = RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp))
-            .clip(RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp)),
     ) {
         NavigationBar(
             modifier = modifier,
