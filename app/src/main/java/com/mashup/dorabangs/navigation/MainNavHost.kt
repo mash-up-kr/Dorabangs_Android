de62187c770caa9d03196b9c645b5908e86605b3
Ahn-seokjoo <tjrwn0716@naver.com>
2024-07-24 01:12:54 +0900

    [feature] 한거 지우기

--- app/src/main/java/com/mashup/dorabangs/navigation/MainNavHost.kt
+++ app/src/main/java/com/mashup/dorabangs/navigation/MainNavHost.kt
@@ -84,7 +84,6 @@ fun MainNavHost(
                 appState.navController.popBackStack()
             },
             finishSaveLink = {
-                // TODO 클릭 때 데이터 스토어에 저장해서 다시 클립보드 안뜨게 하기?
                 appState.navController.navigateToHome(
                     navOptions = navOptions {
                         popUpTo(appState.navController.graph.id) {
