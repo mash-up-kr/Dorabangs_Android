64ad3b2f6dce0e32dcac98bf8d1d8706ecd51449
ddyeon <cab05102@naver.com>
2024-07-24 01:21:10 +0900

    [feat] 홈 튜토리얼 영상 exoplayer적용

---
+++ feature/home/src/main/java/com/mashup/dorabangs/feature/tutorial/TutorialVideo.kt
@@ -0,0 +1,50 @@
+package com.mashup.dorabangs.feature.tutorial
+
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LifecycleStartEffect
+import androidx.media3.common.MediaItem
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.ui.PlayerView
+
+@Composable
+fun TutorialVideo() {
+    val context = LocalContext.current
+    val exoplayer = ExoPlayer.Builder(context).build()
+
+    val mediaItem = MediaItem.fromUri("android.resource://com.mashup.dorabangs.feature.home/")
+    exoplayer.apply {
+        setMediaItem(mediaItem)
+        repeatMode = ExoPlayer.REPEAT_MODE_ALL
+        prepare()
+        playWhenReady = true
+    }
+
+    AndroidView(
+        factory = { factoryContext ->
+            PlayerView(factoryContext).apply {
+                layoutParams = FrameLayout.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+                )
+                this.player = exoplayer
+                this.useController = false
+            }
+        },
+        modifier = Modifier
+            .fillMaxWidth()
+            .aspectRatio(1f)
+    )
+    LifecycleStartEffect(Lifecycle.Event.ON_START) {
+        onStopOrDispose {
+            exoplayer.release()
+        }
+    }
+}
