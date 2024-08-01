db67a96b930fb6eb5fd46758ec24e24b9191f038
ddyeon <cab05102@naver.com>
2024-08-01 22:51:26 +0900

    [fix] 린트 수정

--- core/designsystem/src/main/java/com/mashup/dorabangs/core/designsystem/component/bottomsheet/BottomSheetType.kt
+++ core/designsystem/src/main/java/com/mashup/dorabangs/core/designsystem/component/bottomsheet/BottomSheetType.kt
@@ -6,14 +6,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.SheetValue
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
