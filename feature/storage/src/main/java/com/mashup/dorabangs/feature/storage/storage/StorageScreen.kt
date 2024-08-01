3fabe835c8671ce73366014058f501fd27e94ebc
ddyeon <cab05102@naver.com>
2024-08-01 21:53:35 +0900

    [fix] QA 보관함 탑바 재정렬

--- feature/storage/src/main/java/com/mashup/dorabangs/feature/storage/storage/StorageScreen.kt
+++ feature/storage/src/main/java/com/mashup/dorabangs/feature/storage/storage/StorageScreen.kt
@@ -25,6 +25,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -154,6 +155,7 @@ fun StorageTopAppBar(
             .padding(horizontal = 20.dp, vertical = 12.dp)
             .background(Color.Transparent),
         horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
             text = stringResource(id = R.string.storage),
