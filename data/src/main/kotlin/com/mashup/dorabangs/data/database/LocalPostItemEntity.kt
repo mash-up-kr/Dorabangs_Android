1a38758d2d0c29c65c82a8488bc19c0eefc1dd1f
ddyeon <cab05102@naver.com>
2024-08-11 17:59:38 +0900

    [feat] 룸 데이터베이스 세팅

---
+++ data/src/main/kotlin/com/mashup/dorabangs/data/database/LocalPostItemEntity.kt
@@ -0,0 +1,74 @@
+package com.mashup.dorabangs.data.database
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.mashup.dorabangs.data.model.AIStatusResponseModel
+import com.mashup.dorabangs.data.model.LinkKeywordResponseModel
+import com.mashup.dorabangs.data.model.toDomain
+import com.mashup.dorabangs.domain.model.AIStatus
+import com.mashup.dorabangs.domain.model.LinkKeywordInfo
+import com.mashup.dorabangs.domain.model.Post
+
+
+@Entity
+data class LocalPostItemEntity(
+    @PrimaryKey val id: String = "",
+    val createdAt: String? = "",
+    val description: String? = "",
+    val folderId: String? = "",
+    val isFavorite: Boolean = false,
+    val keywords: List<LinkKeywordResponseModel>? = listOf(),
+    val title: String? = "",
+    val url: String? = "",
+    val userId: String? = "",
+    val thumbnailImgUrl: String? = "",
+    val aiStatusResponseModel: AIStatusResponseModel = AIStatusResponseModel.NOTHING,
+    val readAt: String?,
+)
+
+
+fun Post.toLocalEntity(): LocalPostItemEntity {
+    return LocalPostItemEntity(
+        id = id,
+        createdAt = createdAt,
+        description = description,
+        folderId = folderId,
+        isFavorite = isFavorite,
+        keywords =  keywords?.map { it.toData() },
+        title = title,
+        url = url,
+        thumbnailImgUrl = thumbnailImgUrl,
+        aiStatusResponseModel = aiStatus.toData(),
+        readAt = readAt,
+    )
+}
+
+fun AIStatus.toData() = when (this) {
+    AIStatus.IN_PROGRESS -> AIStatusResponseModel.IN_PROGRESS
+    AIStatus.SUCCESS -> AIStatusResponseModel.SUCCESS
+    AIStatus.NOTHING -> AIStatusResponseModel.NOTHING
+}
+fun LinkKeywordInfo.toData(): LinkKeywordResponseModel {
+    return LinkKeywordResponseModel(
+        id = id,
+        name = name.orEmpty(),
+    )
+}
+
+fun LocalPostItemEntity.toPost(): Post {
+    return Post(
+        id = id,
+        createdAt = createdAt.orEmpty(),
+        description = description.orEmpty(),
+        folderId = folderId.orEmpty(),
+        isFavorite = isFavorite,
+        keywords =  keywords?.map { it.toDomain() },
+        title = title.orEmpty(),
+        url = url.orEmpty(),
+        thumbnailImgUrl = thumbnailImgUrl.orEmpty(),
+        aiStatus = aiStatusResponseModel.toDomain(),
+        readAt = readAt,
+    )
+}
+
+
