fd034b5a0fe435435496dd371cc15efcb1b21086
devdayeon <dani0904@megastudy.net>
2024-10-02 09:15:09 +0900

    [fix] 린트 적용

--- feature/storage/src/main/java/com/mashup/dorabangs/feature/storage/storagedetail/StorageDetailHeader.kt
+++ feature/storage/src/main/java/com/mashup/dorabangs/feature/storage/storagedetail/StorageDetailHeader.kt
@@ -1,18 +1,16 @@
 package com.mashup.dorabangs.feature.storage.storagedetail
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
-import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.mashup.dorabangs.core.designsystem.component.chips.DoraChips
 import com.mashup.dorabangs.core.designsystem.component.chips.FeedUiModel
 import com.mashup.dorabangs.core.designsystem.component.topbar.DoraTopBar
